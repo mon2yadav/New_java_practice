6cc9d89ab5bf90b50444c458249b2f2843a0817f
montu yadav <mon2yadav@gmail.com>
2022-03-29 13:21:00 +0530

    'add'

--- src/java_programs/Calcidemo.java
+++ src/java_programs/Calcidemo.java
@@ -10,29 +10,29 @@ public class Calcidemo {
 	public void add()
 	{
 		
-		System.out.println("please enter two no. for add");
+		System.out.println("Please enter two no. for add");
 		 a=sc.nextInt();
 		 b=sc.nextInt();
 		 c=a+b;
-		 System.out.println("add result");
+		 System.out.println(" Result");
 		 System.out.println(c);
-		 System.out.println("enter two no. for multiply");
+		 System.out.println("Enter two no. for multiply");
 		 a=sc.nextInt();
 		 b=sc.nextInt();
 		 d=a*b;
-		 System.out.println("multiply result");
+		 System.out.println("Result");
 		 System.out.println(d);
-		 System.out.println("enter two no. for divide");
+		 System.out.println("Enter two no. for divide");
 		 a=sc.nextInt();
 		 b=sc.nextInt();
 		 e=a/b;
-		 System.out.println("divide result");
+		 System.out.println("Result");
 		 System.out.println(e);
-		 System.out.println("enter two numbers for substract");
+		 System.out.println("Enter two numbers for substract");
 		 a=sc.nextInt();
 		 b=sc.nextInt();
 		 c=a-b;
-		 System.out.println("subtract result");
+		 System.out.println("Result");
 		 System.out.println(c);
 		 
 	}
