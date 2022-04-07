c922d2ec7e58a1f1b3041c0cfb34793eb0f6d7c0
montu yadav <mon2yadav@gmail.com>
2022-04-07 10:41:39 +0530

    'add'

--- src/java_programs/ForLoop_Demo.java
+++ src/java_programs/ForLoop_Demo.java
@@ -1,13 +1,43 @@
 package java_programs;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ForLoop_Demo {
 
 	public void getdata() {
-		System.out.println();
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println(i+"*7="+i*7);
+		}
+		
 	}
+	public void getchardata() {
+		for(char i='A'; i<='Z'; i++)
+		{
+		System.out.println(i);	
+		}
+	}
+	 
+	public void gettable(int num)
+	{
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println(i*num);
+		}
+	}
+	
+
 	
 	public static void main(String[] args) {
 		ForLoop_Demo ob=new ForLoop_Demo();
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Please enter the number");
+//		int num=sc.nextInt();
+//		ob.gettable(num);
+		ob.getdata(); 
+//		ob.getchardata();
+		
 		
 	}
 }
